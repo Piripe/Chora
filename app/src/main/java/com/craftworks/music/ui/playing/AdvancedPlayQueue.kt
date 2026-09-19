c65a994803d3d57fd1d8dd2e6d63df9711a1fa94
Piripe <pierre.piripe@proton.me>
2026-09-19 19:27:03 +0200

    Added queue item menu with Move Next action

--- app/src/main/java/com/craftworks/music/ui/playing/AdvancedPlayQueue.kt
+++ app/src/main/java/com/craftworks/music/ui/playing/AdvancedPlayQueue.kt
@@ -4,7 +4,7 @@ import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -23,6 +23,8 @@ import androidx.compose.material.icons.rounded.PlayArrow
 import androidx.compose.material3.BottomSheetDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.ListItemDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -38,14 +40,17 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.hapticfeedback.HapticFeedbackType
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalHapticFeedback
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
 import androidx.media3.common.MediaItem
 import androidx.media3.common.Player
 import androidx.media3.common.Timeline
@@ -59,7 +64,6 @@ import com.craftworks.music.data.model.getProvider
 import com.craftworks.music.data.model.id
 import sh.calvin.reorderable.ReorderableItem
 import sh.calvin.reorderable.rememberReorderableLazyListState
-import java.util.UUID
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -79,6 +83,10 @@ fun AdvancedPlayQueueContent(
     val haptic = LocalHapticFeedback.current
 
     val context = LocalContext.current
+
+    var selectedMediaItem: QueueItem? by remember { mutableStateOf(null) }
+    var selectedMediaIndex by remember { mutableIntStateOf(-1) }
+
 
     DisposableEffect(mediaController) {
         fun syncList() {
@@ -160,9 +168,15 @@ fun AdvancedPlayQueueContent(
                     shape = MaterialTheme.shapes.small,
                     modifier = Modifier
                         .fillMaxWidth()
-                        .clickable {
-                            mediaController.seekTo(index, 0)
-                        }
+                        .combinedClickable(
+                            onClick = {
+                                mediaController.seekTo(index, 0)
+                            },
+                            onLongClick = {
+                                selectedMediaIndex = index
+                                selectedMediaItem = item
+                            }
+                        )
                 ) {
                     Row(
                         modifier = Modifier.padding(4.dp),
@@ -267,6 +281,64 @@ fun AdvancedPlayQueueContent(
                     }
                 }
             }
+        }
+    }
+
+    selectedMediaItem?.let {
+        QueueItemMenu(
+            onDialogDismiss = {selectedMediaItem = null},
+            queueItem = it,
+            queueIndex = selectedMediaIndex,
+            mediaController = mediaController
+        )
+    }
+}
+
+@Composable
+private fun QueueMenuButton(icon: Int, text: Int, action: () -> Unit) {
+    ListItem(
+        onClick = action,
+        modifier = Modifier
+            .fillMaxWidth(),
+        colors = ListItemDefaults.colors(
+            containerColor = Color.Transparent
+        ),
+        leadingContent = {
+            Icon(
+                imageVector = ImageVector.vectorResource(icon),
+                contentDescription = stringResource(text)
+            )
+        },
+        content = { Text(stringResource(text)) }
+    )
+}
+
+@Composable
+fun QueueItemMenu(
+    onDialogDismiss: () -> Unit,
+    queueItem: QueueItem,
+    queueIndex: Int,
+    mediaController: MediaController
+) {
+    Dialog(onDismissRequest = { onDialogDismiss() }) {
+        Surface(
+            shape = RoundedCornerShape(16.dp),
+        ) {
+            Box(
+                contentAlignment = Alignment.Center
+            ) {
+                Column(modifier = Modifier.padding(24.dp)) {
+                    QueueMenuButton(R.drawable.play_next_24px, R.string.action_move_next) {
+                        val to = mediaController.currentMediaItemIndex
+                        if (queueIndex > to) {
+                            mediaController.moveMediaItem(queueIndex, to+1)
+                        } else {
+                            mediaController.moveMediaItem(queueIndex, to)
+                        }
+                    }
+                }
+            }
+
         }
     }
 }
